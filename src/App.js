38da0e65851166c1342d3eafaf87b18a8ac13cda
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-01-30 21:26:00 +0100

    Pulling data from API seems to be working. Yay! More work to do yet, though.

--- src/App.js
+++ src/App.js
@@ -4,52 +4,59 @@ import "./App.css";
 export default function App() {
   const [loading, setLoading] = useState(false);
   const [location, setLocation] = useState("");
-  const [weather, setWeather] = useState(null);
+  const [weather, setWeather] = useState({});
   const [error, setError] = useState("");
 
   const handleChange = (e) => {
-    // handle key presses
+    //Not sure what to put here...😅
+    const value = e.target.value;
+    setLocation(value);
   };
 
-  const handleSubmit = (e) => {
-    getWeather();
-  };
   //ASYNC FUNCTION VERSION
   async function getWeather() {
-    let key = "5647a97d7c0510ac58b383eda8e00511";
     let response = await fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        location +
-        "&appid=" +
-        key
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=5647a97d7c0510ac58b383eda8e00511&units=metric`
     );
     let data = await response.json();
-    setLocation(data);
+    console.log(data); //Getting data, ok.
+    setWeather(data); //Not setting weather = data...
   }
-  //FETCH VERSION
-  // const getWeather = () => {
-  //   let key = "5647a97d7c0510ac58b383eda8e00511";
-  //   fetch(
-  //     "https://api.openweathermap.org/data/2.5/weather?q=" +
-  //       location +
-  //       "&appid=" +
-  //       key
-  //   )
-  //     .then((response) => {
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       setLocation(data);
-  //     });
-  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getWeather();
+    console.log(location);
+    console.log(weather);
+    setLocation(""); //empty input field
+  };
 
   return (
     <div>
       <h1>What's the weather?</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" />
+        <label forhtml="location">Location:</label>
+        <input
+          type="text"
+          name="location"
+          className="form-control"
+          placeholder="city name"
+          value={location}
+          onChange={handleChange}
+        />
         <button type="submit">Submit</button>
       </form>
+      {weather && (
+        <div className="container">
+          {location}
+          {weather.main.temp} degrees
+        </div>
+      )}
     </div>
   );
 }
+// TODO:
+// 1. Put weather area together. Not working yet. Location working, errors with other stuff.
+// 2. Isn't console logging weather with data, but seems to be pulling it.
+// 3. Put code for errors.
+// 4. Look into what to do with loading...not sure.
