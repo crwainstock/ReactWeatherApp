749e494e2534e426f91a2a5c5b15268313d65204
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-01-30 19:56:13 +0100

    created blank React app

---
+++ fetch-assignment/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [loading, setLoading] = useState(false);
+  const [location, setLocation] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    // handle key presses
+  };
+
+  const handleSubmit = (e) => {
+    // handle form submit
+  };
+
+  const getWeather = () => {
+    // call Open Weather API
+  };
+
+  return <div>Hello World!</div>;
+}
